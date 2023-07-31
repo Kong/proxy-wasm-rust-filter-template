aed1fead6e8a931a17b66c9a457971b0be2cf9fa
Hisham Muhammad <hisham@gobolinux.org>
2023-07-31 17:17:16 -0300

    feat(*): update filter to add a greeting header

--- README.md
+++ README.md
@@ -3,9 +3,17 @@ Proxy-Wasm Rust Filter Template
 
 This repository contains a [Proxy-Wasm](https://github.com/proxy-wasm/spec)
 filter that can be used as a reference to quickly start implementing new
-filters. The filter implements the entrypoints and function calls supported by
-WasmX. Please refer to [WasmX docs](https://github.com/Kong/ngx_wasm_module/blob/main/docs/PROXY_WASM.md#supported-entrypoints)
-for the list of supported entrypoints.
+filters in Rust.
+
+This is a "hello-world" type filter performing a minimal operation and showing
+the basics of filter configuration: it simply adds a new header `X-Greeting`
+to a proxied response. You can pass the filter a JSON configuration in the
+format `{ "my_greeting": "Howdy!" }` to change the value of the injected
+header.
+
+Please refer to [WasmX
+docs](https://github.com/Kong/ngx_wasm_module/blob/main/docs/PROXY_WASM.md#supported-entrypoints)
+for the list of supported functions and entrypoints.
 
 Requirements
 ============
--- src/filter.rs
+++ src/filter.rs
@@ -1,4 +1,4 @@
-use log::{info, warn};
+use log::warn;
 use proxy_wasm::{traits::*, types::*};
 use serde::Deserialize;
 use serde_json_wasm::de;
@@ -7,10 +7,10 @@ use serde_json_wasm::de;
 // Config
 // -----------------------------------------------------------------------------
 
-#[derive(Deserialize, Clone, Copy, Debug)]
+#[derive(Deserialize, Clone, Debug)]
 struct Config {
     #[serde(skip_serializing_if = "Option::is_none")]
-    my_status_code: Option<u32>,
+    my_greeting: Option<String>,
 }
 
 // -----------------------------------------------------------------------------
@@ -22,7 +22,6 @@ struct MyFilterRoot {
 }
 
 struct MyFilter {
-    context_id: u32,
     config: Config,
 }
 
@@ -49,9 +48,7 @@ impl RootContext for MyFilterRoot {
 //    fn on_tick(&mut self) {
 //    }
 
-    fn on_configure(&mut self, config_size: usize) -> bool {
-        info!("on_configure, config_size: {}", config_size);
-
+    fn on_configure(&mut self, _config_size: usize) -> bool {
         if let Some(config_bytes) = self.get_plugin_configuration() {
             match de::from_slice::<Config>(&config_bytes) {
                 Ok(config) => {
@@ -79,12 +76,9 @@ impl RootContext for MyFilterRoot {
         Some(ContextType::HttpContext)
     }
 
-    fn create_http_context(&self, context_id: u32) -> Option<Box<dyn HttpContext>> {
-        info!("create_http_context: context_id: {}", context_id);
-
+    fn create_http_context(&self, _context_id: u32) -> Option<Box<dyn HttpContext>> {
         if let Some(config) = &self.config {
             Some(Box::new(MyFilter {
-                context_id: context_id,
                 config: config.clone(),
             }))
         } else {
@@ -116,12 +110,10 @@ impl HttpContext for MyFilter {
 //        Action::Continue
 //    }
 //
-    fn on_http_response_headers(&mut self, nheaders: usize, _eof: bool) -> Action {
-        info!("#{} on_response_headers, headers: {}", self.context_id, nheaders);
-
-        match self.config.my_status_code {
-            Some(status) => {
-                self.set_http_response_header("status", Some(&status.to_string()))
+    fn on_http_response_headers(&mut self, _nheaders: usize, _eof: bool) -> Action {
+        match &self.config.my_greeting {
+            Some(greeting) => {
+                self.set_http_response_header("X-Greeting", Some(greeting.as_str()))
             },
             None => ()
         }
